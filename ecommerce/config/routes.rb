9f8e7511719b9eca765336fc04c49dd1e7814a78
mathabethakore <syed.tahmid15@gmail.com>
2024-10-10 03:49:17 +0600

    Ecommerce Website with admin panel

---
+++ ecommerce/config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
+  devise_for :admins
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # Defines the root path route ("/")
+  root to: "home#index"
+  #on authentication of an admin_user, route to admin's index page and set it as admin_root key
+  
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+    #when you get admin it jsut goes to the admins main page which is the admin#index page
+  get "admin" => "admin#index" #like website.com/admin will redirect to admin#index basically admin_controller#indexroute
+end
