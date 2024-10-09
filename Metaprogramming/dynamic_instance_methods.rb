4b9d4a2b7bfb5d6427c7295de54cbaa6e6e96428
mathabethakore <syed.tahmid15@gmail.com>
2024-10-10 01:52:17 +0600

    maintenance

--- Metaprogramming/dynamic_instance_methods.rb
+++ Metaprogramming/dynamic_instance_methods.rb
@@ -1,23 +1,25 @@
 class Person2
   
   def method_missing(method_name, *args)
-
     puts "Undefined method: #{method_name}"
-    #ask if method needs to be defined
       puts "Do you want to create this field: Y/N"
       query_field = gets.chomp.upcase.to_s
       if query_field == "Y" 
         puts "Enter value to insert"
         query_field = gets.chomp
         instance_variable_set("@#{method_name}", query_field)
-         # Dynamically define the setter method for this instance only
-         define_singleton_method("#{method_name}=") do |val|
-         instance_variable_set("@#{method_name}", val)
+         
+        # Dynamically define the setter method for this instance only
+          define_singleton_method("#{method_name}=") do |val|
+           instance_variable_set("@#{method_name}", val)
        end
+
         # Dynamically define the getter method for this instance only
         define_singleton_method(method_name) do
           print "#{method_name} is #{instance_variable_get("@#{method_name}")}\n"
         end
+      
+      
       else
         puts "Method Discarded"
       end
